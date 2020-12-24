f145ad48c66f863968d16a926ba1d650bcf9b4f4
23564 <707280427@qq.com>
2020-12-24 14:13:32 +0800

    test

---
+++ src/index.js
@@ -0,0 +1,19 @@
+function foo(a, b, c, d) {
+  a();
+  b();
+  c();
+  d();
+}
+function reallyLongArg() {}
+function omgSoManyParameters() {}
+function IShouldRefactorThis() {}
+
+function isThereSeriouslyAnotherOne() {}
+foo(
+  reallyLongArg(),
+  omgSoManyParameters(),
+  IShouldRefactorThis(),
+  isThereSeriouslyAnotherOne()
+);
+
+const lint = "eslint";
